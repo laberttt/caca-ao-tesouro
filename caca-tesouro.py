d61d2fafc01a417c71801534c9766c318c6d53d3
laberttt <labert.ribeiroo@gmail.com>
2024-10-18 09:46:57 -0300

    versão final

--- caca-tesouro.py
+++ caca-tesouro.py
@@ -4,17 +4,15 @@ import cores
 
 # Inicialização do Pygame e fonte
 pygame.init()
+pygame.mixer.init()
 fonte = pygame.font.SysFont("Comic Sans MS", 30)
 
 # Definições da tela e das células
 largura_tela = 512
 altura_tela = 600
-lado_celula = 50
-altura_indicador = 50
-altura_score = 50
-num_linhas = 4
-num_colunas = 4
-lado_celula_largura = largura_tela // num_colunas
+altura_indicador, altura_score = 50, 50
+num_linhas, num_colunas = 4, 4
+lado_celula = largura_tela // num_colunas
 altura_tabuleiro = altura_tela - altura_indicador - altura_score - 50
 lado_celula_altura = altura_tabuleiro // num_linhas # espaço para o score
 
@@ -28,24 +26,32 @@ imagem_de_fundo = pygame.transform.scale(imagem_de_fundo, (largura_tela, altura_
 
 # Carrega a imagem do tesouro
 imagem_tesouro = pygame.image.load("images/tesouro.png")
-imagem_tesouro = pygame.transform.scale(imagem_tesouro, (lado_celula - 2, lado_celula - 2))  # Ajusta o tamanho conforme necessário
+imagem_tesouro = pygame.transform.scale(imagem_tesouro, (lado_celula - 10, lado_celula_altura - 10))  # Ajusta o tamanho conforme necessário
 
 # Carrega a imagem do buraco
 imagem_buraco = pygame.image.load("images/buraco.png")
-imagem_buraco = pygame.transform.scale(imagem_buraco, (lado_celula - 2, lado_celula - 2))  # Ajusta o tamanho conforme necessário
+imagem_buraco = pygame.transform.scale(imagem_buraco, (lado_celula - 10, lado_celula_altura - 10))  # Ajusta o tamanho conforme necessário
+
+# Função para carregar a música da vitória de algum ganhador
+def musica_ganhador():
+    return pygame.mixer.Sound("songs/ganhador.mp3")
+
+# Função para carregar o som do clique nas células
+def clique_celula():
+    return pygame.mixer.Sound("songs/click.mp3")
 
 # Tela inicial
 def desenhar_tela_inicial(tela, imagem_de_fundo):
     tela.blit(imagem_de_fundo, (0, 0))
 
 # Função para desenhar o tabuleiro 16 células (4 x 4)
-def desenhar_tabuleiro(tela, num_linhas, num_colunas, lado_celula_largura, lado_celula_altura):
+def desenhar_tabuleiro(tela, num_linhas, num_colunas, lado_celula, lado_celula_altura):
     for i in range(num_linhas):
         for j in range(num_colunas):
-            pygame.draw.rect(tela, cores.WHITE, (i * lado_celula_largura, j * lado_celula_altura, lado_celula_largura, lado_celula_altura), 4)
+            pygame.draw.rect(tela, cores.WHITE, (i * lado_celula, j * lado_celula_altura, lado_celula, lado_celula_altura), 4)
     pygame.display.update()
 
-# Criando o tabuleiro virtual cujos elementos correspondem a um tesouro ('T')
+# Criando o tabuleiro virtual cujos elementos correspondem a um tesouro ('T') ou um buraco ('B')
 def criar_conteudo_celula(num_linhas, num_colunas):
     conteudo_celula = [[None for _ in range(num_linhas)] for _ in range(num_colunas)]
     return conteudo_celula
@@ -120,12 +126,10 @@ def desenhar_score_e_turno(tela, fonte, score_jogador1, score_jogador2, turno_jo
 # Laço do jogo
 def loop_jogo(tela, imagem_de_fundo, fonte, imagem_tesouro, imagem_buraco, conteudo_celula, celula_revelada, num_linhas, num_colunas, largura_tela, altura_tela, altura_score, altura_indicador):
     jogo_cancelado = False
-    score_jogador1 = 0
-    score_jogador2 = 0
-    turno_jogador1 = True
+    score_jogador1, score_jogador2, turno_jogador1 = 0, 0, True
     
     # Desenhando tabuleiro inicialmente
-    desenhar_tabuleiro(tela, num_linhas, num_colunas, lado_celula_largura, lado_celula_altura)
+    desenhar_tabuleiro(tela, num_linhas, num_colunas, lado_celula, lado_celula_altura)
     pygame.display.update()
     
     while not jogo_cancelado:
@@ -139,8 +143,9 @@ def loop_jogo(tela, imagem_de_fundo, fonte, imagem_tesouro, imagem_buraco, conte
             if evento.type == pygame.MOUSEBUTTONDOWN and evento.button == 1:
                 # Pega as coordenadas do ponto de clique e calcula a célula
                 mouse_x, mouse_y = evento.pos
-                celula_x = mouse_x // lado_celula_largura
+                celula_x = mouse_x // lado_celula
                 celula_y = mouse_y // lado_celula_altura
+                clique_celula().play()
                 
                 # Clicou fora do tabuleiro
                 if celula_y > num_linhas - 1:
@@ -176,7 +181,7 @@ def loop_jogo(tela, imagem_de_fundo, fonte, imagem_tesouro, imagem_buraco, conte
                 tela.blit(imagem_de_fundo, (0, 0))
 
                 # Desenha o tabuleiro
-                desenhar_tabuleiro(tela, num_linhas, num_colunas, lado_celula_largura, lado_celula_altura)
+                desenhar_tabuleiro(tela, num_linhas, num_colunas, lado_celula, lado_celula_altura)
 
                 # Desenha o conteúdo das células reveladas
                 for i in range(num_linhas):
@@ -184,16 +189,16 @@ def loop_jogo(tela, imagem_de_fundo, fonte, imagem_tesouro, imagem_buraco, conte
                         if celula_revelada[i][j]:
                             if conteudo_celula[i][j] == "T":
                                 # Mostrar tesouro
-                                tela.blit(imagem_tesouro, (i * lado_celula_largura + 0.3 * lado_celula_largura, j * lado_celula_altura + 0.3 * lado_celula_altura))
+                                tela.blit(imagem_tesouro, (i * lado_celula + 5, j * lado_celula_altura + 5))
                             elif conteudo_celula[i][j] == "B":
                                 # Mostrar Buraco
-                                tela.blit(imagem_buraco, (i * lado_celula_largura + 0.3 * lado_celula_largura, j * lado_celula_altura + 0.3 * lado_celula_altura))
+                                tela.blit(imagem_buraco, (i * lado_celula + 5, j * lado_celula_altura + 5))
                             else:
                                 # Mostrar número de tesouros ao redor
                                 texto_numero = fonte.render(str(conteudo_celula[i][j]), True, cores.WHITE)
-                                tela.blit(texto_numero, (i * lado_celula_largura + 0.4 * lado_celula_largura, j * lado_celula_altura + 0.4 * lado_celula_altura))
+                                tela.blit(texto_numero, (i * lado_celula + 0.4 * lado_celula, j * lado_celula_altura + 0.4 * lado_celula_altura))
                         # Desenha o retângulo da célula
-                        pygame.draw.rect(tela, cores.WHITE, (i * lado_celula_largura, j * lado_celula_altura, lado_celula_largura, lado_celula_altura), 4)
+                        pygame.draw.rect(tela, cores.WHITE, (i * lado_celula, j * lado_celula_altura, lado_celula, lado_celula_altura), 4)
 
                 # Mostra o score na parte inferior da tela
                 desenhar_score_e_turno(tela, fonte, score_jogador1, score_jogador2, turno_jogador1, largura_tela, altura_tela, altura_score, altura_indicador)
@@ -205,8 +210,10 @@ def loop_jogo(tela, imagem_de_fundo, fonte, imagem_tesouro, imagem_buraco, conte
                     # Mostrar o ganhador diretamente na tela
                     if score_jogador1 > score_jogador2:
                         texto_ganhador = fonte.render("Jogador 1 GANHOU", True, cores.WHITE)
+                        musica_ganhador().play()
                     elif score_jogador2 > score_jogador1:
                         texto_ganhador = fonte.render("Jogador 2 GANHOU", True, cores.WHITE)
+                        musica_ganhador().play()
                     else:
                         texto_ganhador = fonte.render("EMPATE", True, cores.WHITE)
 
@@ -221,7 +228,7 @@ def loop_jogo(tela, imagem_de_fundo, fonte, imagem_tesouro, imagem_buraco, conte
 # Função principal para iniciar o jogo
 def main():
     desenhar_tela_inicial(tela, imagem_de_fundo)
-    desenhar_tabuleiro(tela, num_linhas, num_colunas, lado_celula_largura, lado_celula_altura)
+    desenhar_tabuleiro(tela, num_linhas, num_colunas, lado_celula, lado_celula_altura)
     conteudo_celula = criar_conteudo_celula(num_linhas, num_colunas)
     marcar_tesouros(conteudo_celula, num_linhas, num_colunas)
     marcar_buracos(conteudo_celula, num_linhas, num_colunas)
